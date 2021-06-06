61b8fad31e7c94a9b03589bc0e04d538551cab32
aniruddh <aniruddh622003@gmail.com>
2021-06-06 14:50:09 +0530

    Completed all experiments

---
+++ Lab 8/lab_8.c
@@ -0,0 +1,28 @@
+#include <conio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+  
+void main()
+{
+    int check;
+    char* dirname = "geeskforgeeks";
+    clrscr();
+  
+    check = mkdir(dirname,0777);
+  
+    // check if directory is created or not
+    if (!check)
+        printf("Directory created\n");
+    else {
+        printf("Unable to create directory\n");
+        exit(1);
+    }
+  
+    getch();
+  
+    system("dir");
+    getch();
+}
